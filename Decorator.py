0b84073af0aa9998e073102bc9d41792e513e9a4
Virendrasinh Parmar <parmarvirendra159@gmail.com>
2026-03-08 12:29:53 +0530

    Create Decorator.py
    Decorator Assignment = 08-03-2026

---
+++ Decorator.py
@@ -0,0 +1,32 @@
+def discount_decorator(func):
+
+    def wrapper(base_price):
+        price = func(base_price)
+        print("This is discount decorator")       
+        discount_amount = price * 10 / 100
+        discount_price = price - discount_amount
+        print(discount_price)
+        return discount_price
+    
+    return wrapper
+
+def tax_decorator(func):
+
+    def wrapper(base_price):
+        price = func(base_price)
+        print("This is tax decorator")       
+        discount_amount = price * 18 / 100
+        final_price = price - discount_amount
+        print(final_price)
+        return final_price
+    
+    return wrapper
+
+@tax_decorator
+@discount_decorator
+def calcualte_flight_price(base_price):
+    
+    print("Base Price = ", base_price)
+    return base_price
+
+print(calcualte_flight_price(5000))
